be016f9680e037e69ed976685c4e2ad78950189a
mukil <mukilathuruvan5241@gmail.com>
2023-02-21 10:31:15 +0530

    named import and export

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
+import Header, { Footer, MainContent } from "./components/Header";
 function App() {
   return (
     <div className="App">
       <Header />
       <Header />
       <Header />
-      <Header />
-      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -4,5 +4,12 @@ const Header = () => {
   return <h1>First Component</h1>;
 };
 
-
 export default Header;
+
+export const Footer = () => {
+  return <h4>Footer Component</h4>;
+};
+
+export const MainContent = () => {
+  return <h3>Main Content Component</h3>;
+};
