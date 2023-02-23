59c7cb30e6587e7b9c521708396b78e054ca9d15
mukil <mukilathuruvan5241@gmail.com>
2023-02-23 12:30:11 +0530

    button variants with props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
-import Todo from "./components/condinal rendering/Todo";
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-      <Todo status/>
+      <Button variant={"primary"} rounded size={"sm"}>
+        Primary
+      </Button>
+      <Button variant={"primary"} rounded size={"md"}>
+        Primary
+      </Button>
+      <Button variant={"primary"} rounded size={"lg"}>
+        Primary
+      </Button>
+      <Button variant={"secondary"} rounded size={"sm"}>
+        secondary
+      </Button>
+      <Button variant={"secondary"} rounded size={"md"}>
+        secondary
+      </Button>
+      <Button variant={"secondary"} rounded size={"lg"}>
+        secondary
+      </Button>
+      <Button variant={"danger"} rounded size={"sm"}>
+        danger
+      </Button>
+      <Button variant={"danger"} rounded size={"md"}>
+        danger
+      </Button>
+      <Button variant={"danger"} rounded size={"lg"}>
+        danger
+      </Button>
     </div>
   );
 }
--- my-app/src/components/Button.jsx
+++ my-app/src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import styles from "../style/button.module.css";
-
-const Button = ({ children }) => {
-  return <button className={styles.primary}>{children}</button>;
+import "../style/button.css";
+const Button = ({ children, rounded, variant, size }) => {
+  return (
+    <button
+      className={` button ${variant ? variant : ""} ${rounded ? "rounded" : ""} 
+      ${size ? size : ""}
+      `}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
