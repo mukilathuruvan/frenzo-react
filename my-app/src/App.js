341d75e7dfa0d19593c175c02e55450cdc0910c6
mukil <mukilathuruvan5241@gmail.com>
2023-02-23 12:48:10 +0530

    dynamic button variants

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,16 @@
 import "./App.css";
 import Button from "./components/Button";
+
+import { data } from "./components/data";
+
 function App() {
   return (
     <div className="App">
-      <Button variant={"primary"} rounded size={"sm"}>
-        Primary
-      </Button>
-      <Button variant={"primary"} rounded size={"md"}>
-        Primary
-      </Button>
-      <Button variant={"primary"} rounded size={"lg"}>
-        Primary
-      </Button>
-      <Button variant={"secondary"} rounded size={"sm"}>
-        secondary
-      </Button>
-      <Button variant={"secondary"} rounded size={"md"}>
-        secondary
-      </Button>
-      <Button variant={"secondary"} rounded size={"lg"}>
-        secondary
-      </Button>
-      <Button variant={"danger"} rounded size={"sm"}>
-        danger
-      </Button>
-      <Button variant={"danger"} rounded size={"md"}>
-        danger
-      </Button>
-      <Button variant={"danger"} rounded size={"lg"}>
-        danger
-      </Button>
+      {data.map((text) => (
+        <Button key={text.id} rounded variant={text.varaint} size={text.size}>
+          {text.text}
+        </Button>
+      ))}
     </div>
   );
 }
---
+++ my-app/src/components/data.js
@@ -0,0 +1,6 @@
+export const data = [
+  { id: 1, text: "primary", varaint: "primary", size: "md" },
+  { id: 2, text: "secondary", varaint: "secondary", size: "lg" },
+  { id: 3, text: "danger", varaint: "danger", size: "lg" },
+  { id: 4, text: "primary", varaint: "primary", size: "lg" },
+];
