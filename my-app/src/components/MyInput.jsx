53ffdcb41842bd5614f68c42a56ad418ab7366d2
mukil <mukilathuruvan5241@gmail.com>
2023-03-01 11:04:31 +0530

    controlled elements

--- my-app/src/components/MyInput.jsx
+++ my-app/src/components/MyInput.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 const MyInput = () => {
-  const getValue = () => {
-    console.log(document.getElementById("name-input").value);
-  };
-
+  const [name, setName] = useState("manfree");
+  const handleChange = (e) => setName(e.target.value);
   return (
     <div>
-      <input type="text" id="name-input" />
-      <Button variant={"secondary"} size={"lg"} rounded handler={getValue}>
+      <input type="text" value={name} onChange={handleChange} />
+      <Button
+        variant={"secondary"}
+        size={"lg"}
+        rounded
+        handler={() => console.log(name)}>
         Click
       </Button>
+      <h3>{name}</h3>
     </div>
   );
 };
