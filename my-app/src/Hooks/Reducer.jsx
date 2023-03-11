f23c7cd92c9d73799f80557daf81dfe5303c160c
mukil <mukilathuruvan5241@gmail.com>
2023-03-11 12:18:01 +0530

    custom hook

--- my-app/src/Hooks/Reducer.jsx
+++ my-app/src/Hooks/Reducer.jsx
@@ -1,32 +1,57 @@
 import React, { useReducer } from "react";
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case "increment": {
-      return state + action.payload;
+    case "orderIce": {
+      if (action.payload < 5) {
+        return {
+          ...state,
+          iceCount: state.iceCount - action.payload,
+          amount: 0,
+        };
+      } else if (action.payload >= 5) {
+        let cake = parseInt(action.payload / 5);
+        return {
+          cakeCount: state.cakeCount - cake,
+          iceCount: state.iceCount - action.payload,
+          amount: 0,
+        };
+      }
+      break;
     }
-    case "decrement": {
-      return state - action.payload;
+
+    case "orderCake": {
+      return { ...state, cakeCount: state.cakeCount - action.payload };
+    }
+    case "changeAmount": {
+      return { ...state, amount: action.payload };
     }
     default:
       return state;
   }
 };
+
 const Reducer = () => {
-  const [count, dispatch] = useReducer(reducer, 10);
+  const [stock, dispatch] = useReducer(reducer, {
+    cakeCount: 10,
+    iceCount: 50,
+    amount: 0,
+  });
   return (
     <div>
-      <h1>Reducer -{count}</h1>
-      <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
-        Increment
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
-        Decrement
-      </button>
-      <button onClick={() => dispatch({ type: "increment", payload: 5 })}>
-        Increment-5
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", payload: 5 })}>
-        Decrement-5
+      <input
+        type="number"
+        onChange={(e) =>
+          dispatch({ type: "changeAmount", payload: Number(e.target.value) })
+        }
+      />
+      <h1>
+        cakeCount - {stock.cakeCount} - iceCount - {stock.iceCount}
+      </h1>
+
+      <button
+        onClick={() => dispatch({ type: "orderIce", payload: stock.amount })}>
+        OrderIce
       </button>
     </div>
   );
