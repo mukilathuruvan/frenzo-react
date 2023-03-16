01f18c80673930dbf6fd8b4c9df9768329791532
mukil <mukilathuruvan5241@gmail.com>
2023-03-16 13:10:50 +0530

    configuring routes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ExpenceContext from "./context/ExpenceContext";
-
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./Routes/NavBar";
+import AppRoutes from "./Routes/AppRoutes";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ExpenceContext>
-    <App />
-  </ExpenceContext>
+  <BrowserRouter>
+    <NavBar />
+    <AppRoutes />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
