458829a2d99e2e16f0627d4b61780654bacf2a55
mukil <mukilathuruvan5241@gmail.com>
2023-03-22 15:45:35 +0530

    data ready

--- my-app/src/Routes/AppRoutes.jsx
+++ my-app/src/Routes/AppRoutes.jsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import CardContainer from "../pages/CardContainer";
+import ProductsPage from "../pages/ProductsPage";
 const HomePage = React.lazy(() => import("../pages/HomePage"));
 const UserPage = React.lazy(() => import("../pages/UserPage"));
 
@@ -15,6 +17,11 @@ const AppRoutes = () => {
           </Suspense>
         }
       />
+
+      <Route path="products" element={<ProductsPage />}>
+        <Route path=":category" element={<CardContainer />} />
+      </Route>
+
       <Route path="/*" element={<h1>404 Not Found Error</h1>} />
     </Routes>
   );
--- my-app/src/Routes/NavBar.jsx
+++ my-app/src/Routes/NavBar.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const routes = [
-  "home",
-  "users",
-  "order",
-  "history",
-  "profile",
-  "login",
-  "users-params",
-];
+const routes = ["home", "users", "products", "history", "profile", "login", "users-params"];
 
 const Navbar = () => {
   return (
     <nav>
       {routes.map((route) => (
-        <NavLink
-          key={route}
-          to={route === "home" ? "/" : route}
-          style={{ marginLeft: "30px" }}>
+        <NavLink key={route} to={route === "home" ? "/" : route} style={{ marginLeft: "30px" }}>
           {route}
         </NavLink>
       ))}
