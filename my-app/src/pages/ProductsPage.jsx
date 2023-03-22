4b6957e5ecb0d12657c1539a7bd441713eb53c6f
mukil <mukilathuruvan5241@gmail.com>
2023-03-22 15:47:53 +0530

    data setted up

--- my-app/src/pages/ProductsPage.jsx
+++ my-app/src/pages/ProductsPage.jsx
@@ -1,17 +1,7 @@
-import React, { useEffect } from "react";
-import { data } from "../assets/data";
-import connect from "../context/connect";
-const createProduct = async (product) => {
-  const response = await connect.post("/products", product);
-  console.log(response);
-};
+import React from "react";
+
 const ProductsPage = () => {
-  useEffect(() => {
-    data.forEach((product) => {
-      createProduct(product);
-    });
-  }, []);
-  return <div></div>;
+  return <div>ProductsPage</div>;
 };
 
 export default ProductsPage;
